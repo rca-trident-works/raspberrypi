3b294091e064048c65fb50df359a47fd8adb6dbe
rca <_@r0m.me>
2025-10-10 16:21:26 +0900

    update python/sample6_1

--- 10_python/sample6_1.py
+++ 10_python/sample6_1.py
@@ -1,9 +1,13 @@
 # coding: utf-8
 
-a = 20
+a = 6
 
 # 変数aが10以上であればTru
 if a >= 10:
     print("if文の条件を満たしています")
+elif a < 6:
+    print("elif文の条件を満たしています")
+else:
+    print("else文の処理が実行されます")
 
 print("処理が終了しました")
