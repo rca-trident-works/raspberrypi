cc04c58d5736011c0593cef63a30836b767546ba
rca <_@r0m.me>
2025-10-17 15:56:31 +0900

    improve

--- 12_電子工作とプログラミング/1_プログラムによる電子制御/led_exercise3.py
+++ 12_電子工作とプログラミング/1_プログラムによる電子制御/led_exercise3.py
@@ -13,37 +13,37 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN_1 = 23 # Red
-LED_PIN_2 = 24 # Green
-LED_PIN_3 = 25 # Yellow
+LED_PIN_R = 23 # Red
+LED_PIN_G = 24 # Green
+LED_PIN_Y = 25 # Yellow
 
 GPIO.setwarnings(False)
 GPIO.cleanup()
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN_1, GPIO.OUT)
-GPIO.setup(LED_PIN_2, GPIO.OUT)
-GPIO.setup(LED_PIN_3, GPIO.OUT)
+GPIO.setup(LED_PIN_R, GPIO.OUT)
+GPIO.setup(LED_PIN_G, GPIO.OUT)
+GPIO.setup(LED_PIN_Y, GPIO.OUT)
 
 while True:
     # 赤色LED点灯
-    GPIO.output(LED_PIN_1, GPIO.HIGH)
+    GPIO.output(LED_PIN_R, GPIO.HIGH)
     time.sleep(3)
     # 赤色LED消灯、緑色LED点灯
-    GPIO.output(LED_PIN_1, GPIO.LOW)
-    GPIO.output(LED_PIN_2, GPIO.HIGH)
+    GPIO.output(LED_PIN_R, GPIO.LOW)
+    GPIO.output(LED_PIN_G, GPIO.HIGH)
     time.sleep(5)
     # 緑色LED点滅
     end_time = time.time() + 5
     while time.time() < end_time:
-        GPIO.output(LED_PIN_2, GPIO.LOW)
+        GPIO.output(LED_PIN_G, GPIO.LOW)
         time.sleep(0.5)
-        GPIO.output(LED_PIN_2, GPIO.HIGH)
+        GPIO.output(LED_PIN_G, GPIO.HIGH)
         time.sleep(0.5)
     # 緑色LED消灯、黄色LED点灯
-    GPIO.output(LED_PIN_2, GPIO.LOW)
-    GPIO.output(LED_PIN_3, GPIO.HIGH)
+    GPIO.output(LED_PIN_G, GPIO.LOW)
+    GPIO.output(LED_PIN_Y, GPIO.HIGH)
     time.sleep(2)
     # 黄色LED消灯、赤色LED点灯へ戻る
-    GPIO.output(LED_PIN_3, GPIO.LOW)
+    GPIO.output(LED_PIN_Y, GPIO.LOW)
 
